028f342cedbfa3e6fec0f0a8aa7d2a6d4be362d8
Robin <robin.robinbaby007@gmail.com>
2023-11-22 19:28:15 +0530

    popup design changed

--- lib/screens/diaglog/add_category_dialog.dart
+++ lib/screens/diaglog/add_category_dialog.dart
@@ -8,60 +8,65 @@ class Dialogs extends CategoryDbFunctionsImpl {
   Future<void> addCategoryDialog(BuildContext context) {
     int categoryType = 0;
 
-    TextEditingController _controller = TextEditingController();
+    TextEditingController controller = TextEditingController();
 
     return showDialog(
-        context: context,
-        builder: (context) {
-          return AlertDialog(
-            title: const Text(
-              "Add Category",
-              style: TextStyle(fontWeight: FontWeight.bold),
-              textAlign: TextAlign.center,
-            ),
-            content: SizedBox(
-              height: 150,
-              child: Column(
-                children: [
-                  TextField(
-                    controller: _controller,
-                    decoration: const InputDecoration(hintText: "Name"),
-                  ),
-                  const SizedBox(height: 10),
-                  DropdownMenu(
-                    hintText: "Choose Type",
-                    width: MediaQuery.of(context).size.width * .60,
-                    dropdownMenuEntries: [
-                      categoryDropList[0],
-                      categoryDropList[1]
-                    ],
-                    onSelected: (type) {
-                      categoryType = type;
-                    },
-                  ),
-                ],
-              ),
-            ),
-            actions: [
-              TextButton(
-                style: TextButton.styleFrom(
-                  backgroundColor: Colors.blue,
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          title: const Text(
+            "Add Category",
+            style: TextStyle(fontWeight: FontWeight.bold),
+            textAlign: TextAlign.center,
+          ),
+          content: SizedBox(
+            height: 150,
+            child: Column(
+              children: [
+                TextField(
+                  controller: controller,
+                  decoration: const InputDecoration(
+                      hintText: "Name",
+                      border: boarderProperty,
+                      enabledBorder: boarderProperty),
                 ),
-                child: const Text(
-                  'Save',
-                  style: TextStyle(color: Colors.white),
+                const SizedBox(height: 10),
+                DropdownMenu(
+                  inputDecorationTheme: const InputDecorationTheme(
+                    enabledBorder: boarderProperty,
+                  ),
+                  hintText: "Choose Type",
+                  width: MediaQuery.of(context).size.width * .60,
+                  dropdownMenuEntries: [
+                    categoryDropList[0],
+                    categoryDropList[1]
+                  ],
+                  onSelected: (type) {
+                    categoryType = type;
+                  },
                 ),
-                onPressed: () {
-                  CategoryModel newCategory = CategoryModel(
-                      name: _controller.text,
-                      type: categoryType,
-                      isAvailable: 1);
-                  addToCategory(newCategory);
-                  Navigator.of(context).pop();
-                },
+              ],
+            ),
+          ),
+          actions: [
+            TextButton(
+              style: TextButton.styleFrom(
+                backgroundColor: Colors.blue,
               ),
-            ],
-          );
-        });
+              child: const Text(
+                'Save',
+                style: TextStyle(color: Colors.white),
+              ),
+              onPressed: () {
+                CategoryModel newCategory = CategoryModel(
+                    name: controller.text, type: categoryType, isAvailable: 1);
+                addToCategory(newCategory);
+                Navigator.of(context).pop();
+              },
+            ),
+          ],
+        );
+      },
+    );
   }
 }
--- lib/utils/const.dart
+++ lib/utils/const.dart
@@ -2,9 +2,11 @@ import 'package:flutter/material.dart';
 
 const String categoryTable = "Category";
 
-
 const List<DropdownMenuEntry> categoryDropList = [
   DropdownMenuEntry(value: 0, label: 'Expense'),
   DropdownMenuEntry(value: 1, label: 'Income'),
-
 ];
+
+const OutlineInputBorder boarderProperty = OutlineInputBorder(
+  borderSide: BorderSide(color: Colors.blue),
+);
